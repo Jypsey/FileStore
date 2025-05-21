d504930f9e66e2e55f8321867a5dcdc6dc6241cf
Adish Suresh <adishsuresh3646@gmail.com>
2025-05-21 18:55:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ DATABASE_URL="mongodb+srv://adish:adish@cluster0.mjagcot.mongodb.net/?retryWrite
 API_ID=22359038
 API_HASH="b3901895dc193c30c808ba4f1b550ed0"
 BOT_TOKEN="8033667019:AAGZysXshlJIQh1VloDFEqgA0SKtjPDUfiw"
+ADMINS=5531461861
